aba548652c3b91053fdb663ef14691084d041cc7
0beron <robin.fairey@gmail.com>
2025-03-08 22:04:41 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { FilterableSetsGrid, keyedLocalStorage } from '@flyingcatband/tunebook';
+
+	let { data } = $props();
+	const visualTranspose = keyedLocalStorage('globalTransposition', 0);
+</script>
+
+<svelte:head>
+	<title>MyTunebook</title>
+</svelte:head>
+
+<h1 class="mt-3">Welcome to MyTunebook</h1>
+<p class="mx-auto w-fit flex flex-wrap gap-2">
+	<button onclick={() => ($visualTranspose = 0)}>Make the folder C</button>
+	<button onclick={() => ($visualTranspose = 2)}>Make the folder B♭</button>
+	<button onclick={() => ($visualTranspose = -3)}>Make the folder E♭</button>
+</p>
+<FilterableSetsGrid folder={data.folder} tuneFont="My Custom Font" />
---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { ViewSet } from '@flyingcatband/tunebook';
+	let { data } = $props();
+</script>
+
+<svelte:head>
+	<title>{data.set.name} | MyTunebook</title>
+</svelte:head>
+<button class="z-10 fixed left-2" onclick={() => history.back()}>Back</button>
+<ViewSet
+	set={data.set}
+	displayAbcFields="TNC"
+	folderName={data.folder.name}
+	fontFamily="My Custom Font"
+>
+	{#each data.set.notes as note (note)}
+		<p class="italic">{note}</p>
+	{/each}
+</ViewSet>
