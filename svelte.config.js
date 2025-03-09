210c62bef0f389404397fecbd4ea57db0f8b3106
0beron <robin.fairey@gmail.com>
2025-03-09 02:04:18 +0000

    Looking hopeful, also hardcode in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-      			base: process.env.BASE_PATH || '/ccas-tunebook'
+      			base: '/ccas-tunebook'
     		},
 	},
 	vitePlugin: {
